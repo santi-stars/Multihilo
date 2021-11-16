5264bba0709115397a249adcdca962c97cb19f08
Santi Stars <santi_r6@hotmail.es>
2021-11-16 20:18:45 +0100

    Clase 13/11 poca cosa...

---
+++ multidescarga/src/main/java/com/svalero/multihilo/AppController.java
@@ -0,0 +1,80 @@
+package com.svalero.multihilo;
+
+import com.svalero.multihilo.util.R;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class AppController {
+
+    public TextField tfUrl;
+    public Button btDownload;
+    public TabPane tpDownloads;
+
+    private Map<String, DownloadController> allDownloads;
+
+    public AppController() {
+        allDownloads = new HashMap<>();
+    }
+
+    @FXML
+    public void launchDownload(ActionEvent event) {
+        String urlText = tfUrl.getText();
+        tfUrl.clear();
+        tfUrl.requestFocus();
+
+        launchDownload(urlText);
+    }
+
+    private void launchDownload(String url) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(R.getUI("download.fxml"));
+
+            DownloadController downloadController = new DownloadController(url);
+            loader.setController(downloadController);
+            VBox downloadBox = loader.load();
+
+            String filename = url.substring(url.lastIndexOf("/") + 1);
+            tpDownloads.getTabs().add(new Tab(filename, downloadBox));
+
+            allDownloads.put(url, downloadController);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void stopAllDownloads() {
+    for(DownloadController downloadController:allDownloads.values())
+        downloadController.stop();
+    }
+
+    @FXML
+    public void readDLC() {
+        /*
+        FileChooser fileChooser = new FileChooser();
+            File file = fileChooser.showSaveDialog(tfUrl.getScene().getWindow());
+            if (file == null)
+                return;     // Todo esto pide un fichero
+         */
+        // Leo el fichero y cargo cada linea en un list (clase Files)
+        // Files.read lee cada linea de un fichero
+        // Para cada linea llamar al metodo launchDownload
+    }
+}
