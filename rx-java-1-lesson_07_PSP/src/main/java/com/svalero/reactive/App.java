8010e064ed1922e03551a8332c9cc57afe583f2d
Santi Stars <santi_r6@hotmail.es>
2023-04-25 00:12:12 +0200

    RX Java

--- rx-java-1-lesson_07_PSP/src/main/java/com/svalero/reactive/App.java
+++ rx-java-1-lesson_07_PSP/src/main/java/com/svalero/reactive/App.java
@@ -6,8 +6,12 @@ import io.reactivex.functions.Consumer;
 public class App {
        
         public static void main(String[] args) {
+                // Creamos un observable de Strings con 3 argumentos
                 Observable<String> movies = Observable.just("As Bestas", "Cinco Lobitos", "Alcatraz");
+                // Creamos el consumidor que todo lo que recibe lo imprime
                 Consumer<String> netflixUser = (x) -> System.out.println(x.toUpperCase());
+                // Subscribimos el consumidor al observable
                 movies.subscribe(netflixUser);
         }
-}
+}               // EJECUTAR:    mvn clean package
+                //              java -jar target/rx-example-1.0-SNAPSHOT-jar-with-dependencies.jar
